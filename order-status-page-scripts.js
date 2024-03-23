7b921fe2a51de9fa6875fbae752aaeca85c89d03
Ashikur Rahman <32567010+ashik3g@users.noreply.github.com>
2024-03-24 01:28:14 +0600

    Update order-status-page-scripts.js

--- order-status-page-scripts.js
+++ order-status-page-scripts.js
@@ -3,16 +3,16 @@
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NS5WPD3G');</script>
+})(window,document,'script','dataLayer','GTM-XXXX');</script>
 <!-- End Google Tag Manager -->
 
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NS5WPD3G"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXX"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
 <!-- Global site tag (gtag.js) - Google Analytics  -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-CNJPL62F9L"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXX"></script>
 {% if first_time_accessed %}
 <script>
   window.dataLayer = window.dataLayer || [];
